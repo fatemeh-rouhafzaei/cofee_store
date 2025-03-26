1bf8c8ef7ccad7a11888b701b4564c7cb67da522
Hamzeh <h.qorbani7@gmail.com>
2025-03-26 18:07:40 +0330

    edit for path argument

--- packages/lib/rw_files/rw_files.py
+++ packages/lib/rw_files/rw_files.py
@@ -1,14 +1,14 @@
 import json
 class RWFiles:
-    def __init__(self , file_name):
-        self.file_name = file_name
+    def __init__(self):
+        pass
 
-    def read_json(self):
+    def read_json(self, path):
         # Opening JSON file
-        with open(self.file_name, 'r' , encoding= 'utf-8') as openfile:
+        with open(path, 'r' , encoding= 'utf-8') as openfile:
             # Reading from json file
             return json.load(openfile)
 
-    def write_json(self, data):
-        with open(self.file_name, "w", encoding= 'utf-8') as outfile:
+    def write_json(self, data , path):
+        with open(path, "w", encoding= 'utf-8') as outfile:
             json.dump(data, outfile,ensure_ascii=False, indent= 4)
